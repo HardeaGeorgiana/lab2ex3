829eb9d38a3f94faadd3b4e1704088a3481d4af5
Tenie Alexandru <alexandru_ale26@yahoo.com>
2022-12-05 21:12:45 +0200

    Add project files.

---
+++ lab2ex3/Program.cs
@@ -0,0 +1,41 @@
+﻿
+using System;
+
+namespace lab2ex3
+{
+    /*  Să se scrie un program care citeşte de la tastatură un şir de n numere 
+        naturale şi determină media aritmetică a celor pare, n fiind citit de la 
+        tastatra
+    */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cate numere sa contina sirul? ");
+            int n = int.Parse(Console.ReadLine());
+            int[] sirNumere = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Introduceti numarul din pozitia {i} a sirului de numere: ");
+                int num = int.Parse(Console.ReadLine());
+                sirNumere[i] = num;
+            }
+
+
+            int sumaNumerePare = 0;
+            double contorNumerePare = 0;
+
+            for(int i = 0; i < n; i++)
+            {
+                if (sirNumere[i] % 2 == 0)
+                {
+                    sumaNumerePare = sumaNumerePare + sirNumere[i];
+                    contorNumerePare++;
+                }
+            }
+
+            Console.WriteLine($"Media aritmetica a celor {contorNumerePare} numere pare este {sumaNumerePare / contorNumerePare}");
+        }
+    }
+}
